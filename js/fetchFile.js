26d34d7c31733d27cc55863025a975b644d7736d
Jonathan <jonathan.trhodes07@gmail.com>
2021-08-31 11:23:49 -0500

    button functional in the works

--- js/fetchFile.js
+++ js/fetchFile.js
@@ -12,7 +12,24 @@ const option = {
 
 const MOVIES_URL = "https://oasis-roasted-indigo.glitch.me/movies"
 
-// fetch(MOVIES_URL, option)
-//     .then(response => response.json())
-//     .then(response.map())
-// $.get(MOVIES_URL).done
+    // <!--    initial page loading message-->
+$(window).on('load', function () {
+    $('#loading').hide();
+});
+
+//movies list get request
+const movieList = $.get(MOVIES_URL).done(response =>
+    response.forEach(function(movie){
+        console.log(movie)
+        $('#card1').append(`<ul> <li> ${movie.title} </li> </ul>`)
+    })
+);
+movieList
+//button functionality
+$('#addButton').click(function(e){
+    let addMovie = $('#addedMovie').val()
+    console.log(addMovie)
+    e.preventDefault()
+    $.post(MOVIES_URL, {title:addMovie, rating: '1'})
+        .done()
+})
