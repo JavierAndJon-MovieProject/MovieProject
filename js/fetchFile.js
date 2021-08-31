6a3268793b4f3b6be3d96011d67c2f0a5db93f3d
Javier Guerra <javier.j.guerra4@gmail.com>
2021-08-31 10:05:04 -0500

    Viewing movies array in console

---
+++ js/fetchFile.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const option = {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    // body : JSON.stringify({
+    //
+    // })
+};
+
+const MOVIES_URL = "https://oasis-roasted-indigo.glitch.me/movies"
+
+// fetch(MOVIES_URL, option)
+//     .then(response => response.json())
+//     .then(response.map())
+// $.get(MOVIES_URL).done
